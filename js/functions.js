be7b17f5d0b1767b96564559465c7509ba3e99eb
Uluk <ulix.0500@gmail.com>
2023-07-05 23:03:47 +0600

    GG

---
+++ js/functions.js
@@ -0,0 +1,37 @@
+/*
+const checkStringLength = function (string, length) {
+  if (string.length <= length) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+console.log(checkStringLength('kakaya to stroka', 10))
+
+const checkStringLength = function (string, length) {
+  if (string.length <= length) {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
+
+// checkStringLength('Lets go world', 10)
+//
+// function isPalindrome(str) {
+//   // Удаление всех пробелов из строки
+//   let strippedStr = str.replace(/\s/g, '');
+//
+//   // Приведение строки к нижнему регистру
+//   let lowerCaseStr = strippedStr.toLowerCase();
+//
+//   // Проверка на палиндром
+//   let reversedStr = lowerCaseStr.split('').reverse().join('');
+//   return lowerCaseStr === reversedStr;
+// }
+// console.log(isPalindrome('Топот'))
+// console.log(isPalindrome('kot'))
+*/
