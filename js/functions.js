235003d9fef059e9876d297ac6e660834264c012
U1ix <121996884+U1ix@users.noreply.github.com>
2023-07-07 21:12:58 +0600

    2 задание!

--- js/functions.js
+++ js/functions.js
@@ -1,35 +1,52 @@
- const checkStringLength = function (string, length) {
-   if (string.length <= length) {
-     return true;
-   }
-   else {
-     return false;
-   }
- }
- checkStringLength('Lets go world', 10)
- console.log(checkStringLength('kakaya to stroka', 20))
+// Cтрока короче 20 символов
+const checkStringLength = function (string, length) {
+  if (string.length <= length) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
- /*const checkStringLength = function (string, length) {
-   if (string.length <= length) {
-     return true;
-   }
-   else {
-     return false;
-   }
- }*/
-//
-/*
-function isPalindrome(str) {
-  // Удаление всех пробелов из строки
-  let strippedStr = str.replace(/\s/g, '');
-//
-  // Приведение строки к нижнему регистру
-  let lowerCaseStr = strippedStr.toLowerCase();
-//
-  // Проверка на палиндром
-  let reversedStr = lowerCaseStr.split('').reverse().join('');
-  return lowerCaseStr === reversedStr;
-}
-console.log(isPalindrome('Топот'))
-console.log(isPalindrome('kot'))
-*/
+checkStringLength('Какая то строка', 20);
+
+// Длина строки ровно 18 символов
+const checkLength = function (string, length) {
+  if (string.length === length) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+checkLength('123456789011123456', 18);
+
+// Строка длиннее 10 символов
+const string10 = function (string, length) {
+  if (string.length >= length) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+string10('12345678910', 10);
+
+// Проверка на палиндромом
+const isPalindrome = function (str) {
+  const normalized = str.replaceAll('','').toUpperCase();
+  let result = '';
+  for (let i = normalized.length - 1; i >= 0; i--) {
+    result += normalized.at(i);
+  }
+  if (result === normalized) {
+    return true;
+  }
+  else {
+    return false;
+  }
+};
+
+isPalindrome('');
+
+
